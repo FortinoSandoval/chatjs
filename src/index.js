eb280dbf4e89db4593f0e926a1f1828d890d70e3
Fortino <contact@fortinosandoval.com>
2018-04-09 17:23:10 -0700

    code added

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const path = require('path');
+const express = require('express');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio.listen(server);
+
+const mongoose = require('mongoose');
+
+//db connection
+mongoose.connect('mongodb://localhost/chat-db')
+.then(db => console.log('db is connected'))
+.catch(err=>console.log(err));
+
+//settings
+app.set('port', process.env.PORT || 3000);
+
+require('./sockets')(io);
+
+//static files
+app.use(express.static(path.join(__dirname,'public')));
+
+//starting the server
+server.listen(app.get('port'), ()=>{
+	console.log('server on port', app.get('port'));
+});
